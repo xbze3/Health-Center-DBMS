94acbba9b56bc0941069cc6b8350e9846d27e52c
Ezra Minty <ezranminty@gmail.com>
2024-11-19 20:15:03 -0400

    Add AppointmentsAdditionForm

--- CSE2102-Frontend/src/components/AdditionForms/AppointmentsAdditionForm.tsx
+++ CSE2102-Frontend/src/components/AdditionForms/AppointmentsAdditionForm.tsx
@@ -1,182 +1,170 @@
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
 import Row from "react-bootstrap/Row";
 import "../../components-css/addForm.css";
+import "../../components-css/addFormButton.css";
 import * as formik from "formik";
 import * as yup from "yup";
+import { useState } from "react";
 
 function AppointmentsAdditionForm() {
     const { Formik } = formik;
 
     const schema = yup.object().shape({
-        firstName: yup.string().required(),
-        lastName: yup.string().required(),
-        username: yup.string().required(),
-        city: yup.string().required(),
-        state: yup.string().required(),
-        zip: yup.string().required(),
-        terms: yup.bool().required().oneOf([true], "Terms must be accepted"),
+        patientID: yup.string().required(),
+        staffID: yup.string().required(),
+        date: yup.string().required(),
+        time: yup.string().required(),
+        reason_for_visit: yup.string().required(),
     });
 
+    const [isVisible, setIsVisible] = useState(false);
+
+    function toggleAddBox() {
+        setIsVisible((prevState) => !prevState);
+    }
+
     return (
-        <Formik
-            validationSchema={schema}
-            onSubmit={console.log}
-            initialValues={{
-                firstName: "Mark",
-                lastName: "Otto",
-                username: "",
-                city: "",
-                state: "",
-                zip: "",
-                terms: false,
-            }}
-        >
-            {({ handleSubmit, handleChange, values, touched, errors }) => (
-                <Form noValidate onSubmit={handleSubmit} className="addForm">
-                    <div className="addFormDiv">
-                        <Row className="mb-3">
-                            <Form.Group
-                                as={Col}
-                                md="4"
-                                controlId="validationFormik01"
-                            >
-                                <Form.Label>First name</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    name="firstName"
-                                    value={values.firstName}
-                                    onChange={handleChange}
-                                    isValid={
-                                        touched.firstName && !errors.firstName
-                                    }
-                                />
-                                <Form.Control.Feedback>
-                                    Looks good!
-                                </Form.Control.Feedback>
-                            </Form.Group>
-                            <Form.Group
-                                as={Col}
-                                md="4"
-                                controlId="validationFormik02"
-                            >
-                                <Form.Label>Last name</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    name="lastName"
-                                    value={values.lastName}
-                                    onChange={handleChange}
-                                    isValid={
-                                        touched.lastName && !errors.lastName
-                                    }
-                                />
+        <>
+            <div className={isVisible ? "showAddBox" : "hideAddBox"}>
+                <Formik
+                    validationSchema={schema}
+                    onSubmit={console.log}
+                    initialValues={{
+                        patientID: "",
+                        staffID: "",
+                        date: "",
+                        time: "",
+                        reason_for_visit: "",
+                    }}
+                >
+                    {({ handleSubmit, handleChange, values, errors }) => (
+                        <Form
+                            noValidate
+                            onSubmit={handleSubmit}
+                            className="addForm"
+                        >
+                            <div className="addFormDiv">
+                                <Row className="mb-3">
+                                    <Form.Group
+                                        as={Col}
+                                        md="4"
+                                        controlId="validationFormik01"
+                                    >
+                                        <Form.Label>Patient ID</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            name="patientID"
+                                            value={values.patientID}
+                                            placeholder="E.g. 1"
+                                            onChange={handleChange}
+                                            isInvalid={!!errors.patientID}
+                                        />
+                                        <Form.Control.Feedback type="invalid">
+                                            {errors.patientID}
+                                        </Form.Control.Feedback>
+                                    </Form.Group>
+                                    <Form.Group
+                                        as={Col}
+                                        md="4"
+                                        controlId="validationFormik02"
+                                    >
+                                        <Form.Label>Staff ID</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            name="staffID"
+                                            value={values.staffID}
+                                            placeholder="E.g. 23"
+                                            onChange={handleChange}
+                                            isInvalid={!!errors.staffID}
+                                        />
+                                        <Form.Control.Feedback type="invalid">
+                                            {errors.staffID}
+                                        </Form.Control.Feedback>
+                                    </Form.Group>
+                                    <Form.Group
+                                        as={Col}
+                                        md="4"
+                                        controlId="validationFormik02"
+                                    >
+                                        <Form.Label>Date</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            name="date"
+                                            value={values.date}
+                                            placeholder="Date"
+                                            onChange={handleChange}
+                                            isInvalid={!!errors.date}
+                                        />
+                                        <Form.Control.Feedback type="invalid">
+                                            {errors.date}
+                                        </Form.Control.Feedback>
+                                    </Form.Group>
+                                </Row>
+                                <Row className="mb-3">
+                                    <Form.Group
+                                        as={Col}
+                                        md="6"
+                                        controlId="validationFormik03"
+                                    >
+                                        <Form.Label>Time</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            placeholder="E.g. 12:00:00"
+                                            name="time"
+                                            value={values.time}
+                                            onChange={handleChange}
+                                            isInvalid={!!errors.time}
+                                        />
 
-                                <Form.Control.Feedback>
-                                    Looks good!
-                                </Form.Control.Feedback>
-                            </Form.Group>
-                            <Form.Group
-                                as={Col}
-                                md="4"
-                                controlId="validationFormikUsername"
-                            >
-                                <Form.Label>Username</Form.Label>
-                                <InputGroup hasValidation>
-                                    <InputGroup.Text id="inputGroupPrepend">
-                                        @
-                                    </InputGroup.Text>
-                                    <Form.Control
-                                        type="text"
-                                        placeholder="Username"
-                                        aria-describedby="inputGroupPrepend"
-                                        name="username"
-                                        value={values.username}
-                                        onChange={handleChange}
-                                        isInvalid={!!errors.username}
-                                    />
-                                    <Form.Control.Feedback type="invalid">
-                                        {errors.username}
-                                    </Form.Control.Feedback>
-                                </InputGroup>
-                            </Form.Group>
-                        </Row>
-                        <Row className="mb-3">
-                            <Form.Group
-                                as={Col}
-                                md="6"
-                                controlId="validationFormik03"
-                            >
-                                <Form.Label>City</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="City"
-                                    name="city"
-                                    value={values.city}
-                                    onChange={handleChange}
-                                    isInvalid={!!errors.city}
-                                />
+                                        <Form.Control.Feedback type="invalid">
+                                            {errors.time}
+                                        </Form.Control.Feedback>
+                                    </Form.Group>
+                                    <Form.Group
+                                        as={Col}
+                                        md="3"
+                                        controlId="validationFormik04"
+                                    >
+                                        <Form.Label>
+                                            Reason for Visit
+                                        </Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            name="reason_for_visit"
+                                            value={values.reason_for_visit}
+                                            onChange={handleChange}
+                                            isInvalid={
+                                                !!errors.reason_for_visit
+                                            }
+                                        />
+                                        <Form.Control.Feedback type="invalid">
+                                            {errors.reason_for_visit}
+                                        </Form.Control.Feedback>
+                                    </Form.Group>
+                                </Row>
+                                <Button
+                                    type="submit"
+                                    variant="success"
+                                    className="submitRecordButton"
+                                >
+                                    Add Appointment
+                                </Button>
+                            </div>
+                        </Form>
+                    )}
+                </Formik>
+            </div>
 
-                                <Form.Control.Feedback type="invalid">
-                                    {errors.city}
-                                </Form.Control.Feedback>
-                            </Form.Group>
-                            <Form.Group
-                                as={Col}
-                                md="3"
-                                controlId="validationFormik04"
-                            >
-                                <Form.Label>State</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="State"
-                                    name="state"
-                                    value={values.state}
-                                    onChange={handleChange}
-                                    isInvalid={!!errors.state}
-                                />
-                                <Form.Control.Feedback type="invalid">
-                                    {errors.state}
-                                </Form.Control.Feedback>
-                            </Form.Group>
-                            <Form.Group
-                                as={Col}
-                                md="3"
-                                controlId="validationFormik05"
-                            >
-                                <Form.Label>Zip</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Zip"
-                                    name="zip"
-                                    value={values.zip}
-                                    onChange={handleChange}
-                                    isInvalid={!!errors.zip}
-                                />
-
-                                <Form.Control.Feedback type="invalid">
-                                    {errors.zip}
-                                </Form.Control.Feedback>
-                            </Form.Group>
-                        </Row>
-                        <Form.Group className="mb-3">
-                            <Form.Check
-                                required
-                                name="terms"
-                                label="Agree to terms and conditions"
-                                onChange={handleChange}
-                                isInvalid={!!errors.terms}
-                                feedback={errors.terms}
-                                feedbackType="invalid"
-                                id="validationFormik0"
-                            />
-                        </Form.Group>
-                        <Button type="submit">Submit form</Button>
-                    </div>
-                </Form>
-            )}
-        </Formik>
+            <Button
+                variant="success"
+                className="addFormButton"
+                onClick={toggleAddBox}
+            >
+                {isVisible ? "Close Form" : "Add Record"}
+            </Button>
+        </>
     );
 }
 
